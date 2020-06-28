c49239aed1c5b664a4586dfb3e18ccedeba07478
Ali Yamini <d.aliyamini@gmail.com>
2020-06-29 01:04:52 +0430

    adding typedefs to gamecl.h

--- gamecl.h
+++ gamecl.h
@@ -2,3 +2,16 @@
  Author: Ali Yamini */
 
 
+#pragma once
+#include <random>
+#include <vector>
+#include <array>
+#include <functional>
+
+typedef std::array<bool, 9> ShutBoard;
+typedef std::vector<int> ShutNum;
+typedef std::function<void(ShutBoard& brd)> DispBrdFn;
+typedef std::function<ShutNum(ShutBoard& brd, int diceVal)> InputFn;
+typedef std::function<void()> DispRndFn;
+typedef std::function<int()> GetDiceAmt;
+typedef std::function<void(int)> UpdateDice;
