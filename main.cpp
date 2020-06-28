27c2a37492aa102472bbe0b2f575866685349e2d
Ali Yamini <d.aliyamini@gmail.com>
2020-06-29 01:23:01 +0430

    adding initializer for shutthebox vars.

--- main.cpp
+++ main.cpp
@@ -3,6 +3,17 @@
 
 # include "gamecl.h"
 
+// Meghdar dehi avalie baray callback ha va maghadir mojod darar bakhsh private
+ShutTheBox::ShutTheBox(Newdisp& cbPrintBrd, InputFn& cbGetInput, DispRndFn& cbDispRnd, GetDiceAmt& cbGetDiceAmt, UpdateDice& cbDiceUpdate) : board{true, true, true, true, true, true, true, true, true}
+{
+	cb_printBoardState = cbPrintBrd;
+	cb_GetInput = cbGetInput;
+	cb_DispRnd = cbDispRnd;
+	cb_GetDiceAmt = cbGetDiceAmt;
+	cb_DiceUpdate = cbDiceUpdate;
+	rng.seed(std::random_device()());
+}
+
 ShutTheBox::~ShutTheBox()
 {
 }
