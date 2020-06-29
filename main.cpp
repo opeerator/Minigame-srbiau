d7437468d311732a598d0a325316d89859bea2c1
Ali Yamini <d.aliyamini@gmail.com>
2020-06-30 01:30:00 +0430

    adding entery point to game.

--- main.cpp
+++ main.cpp
@@ -13,6 +13,26 @@
 // halghe asli
 int main()
 {
+	try
+	{
 
+		GetDiceAmt di = getAmtOfDiceToRoll;
+		UpdateDice dp = dicePrint;
+
+		Newdisp pb = printBoard;
+		InputFn gi = getInput;
+		DispRndFn dr = dispRound;
+
+        // Shoroe game
+		ShutTheBox marhale1(pb, gi, dr, di, dp);
+		std::cout << "Emtiaze shoma: " << marhale1.Start() << std::endl;
+	}
+	catch (const std::exception& ex)
+	{
+		std::cout << "Program: " << ex.what() << std::endl;
+	}
+
+	system("stop");
+	return 0;
 	return 0;
 }
