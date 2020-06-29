a587630408a525da61984ab0cae471010f9f517e
Ali Yamini <d.aliyamini@gmail.com>
2020-06-30 01:25:03 +0430

    main file setup.

---
+++ main.cpp
@@ -0,0 +1,18 @@
+/* This is my implementation of shut the box game for OOP class at srbiau with Ms.zamani
+ Author: Ali Yamini */
+
+#include "gamecl.h"
+#include <functional>
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <sstream>
+
+
+// halghe asli
+int main()
+{
+
+	return 0;
+}
